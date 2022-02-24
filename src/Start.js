4cdc11a975a7d14a2c311033871acafc8ec9e432
son <son@sonui-MacBookAir.local>
2022-02-24 15:43:12 +0900

    result page modify

--- src/Start.js
+++ src/Start.js
@@ -12,33 +12,35 @@ function Start ({nextQuestion}) {
         width:100vw;
         height:100vh;
         background-color:#f7f7f7;
-        color:#fff;
+        overflow:hidden;
 
         .intro_title_box{
             width:100%;
             height:35%;
+            font-size:1.25rem;
+            font-weight:700;
+            margin:5rem 1rem 1rem 5rem;
         }
 
-        .intro_title{
-            width:240px;
-            height:100%;
-            background:url(${title});
-            background-size:contain;
-            background-repeat:no-repeat;
-            margin:5rem 0rem 0rem 2rem;
+        .intro_title_md{
+            width:100%;
+            height:35%;
+            font-size:2rem;
+            font-weight:700;
+            margin-top:30px;
         }
 
         .intro_bg_box{
             width:100%;
             height:65%;
-            background:url(${intro_bg});
         }
 
 
         button{
-            position:fixed;
+            position:absolute;
             bottom:0px;
             width:100%;
+            min-height:56px;
             height:56px;
             color:#fff;
             font-weight:600;
@@ -54,15 +56,25 @@ function Start ({nextQuestion}) {
         }
     `;
 
+    const UnderText = styled.div`
+        position:absolute;
+        width:100%;
+        bottom:100px;
+        font-size:0.8rem;
+        text-align:center;
+    `
+
     return (
         <StartPage>
             <div className="intro_title_box">
-                <div className="intro_title"></div>
-            </div>
-            <div className="intro_bg_box">
-                <div className="intro_bg_img">
+                <div className="intro_title">내 생각을 말해준 대선후보는 누구?</div>
+                <div className="intro_title_md">간단 질문을 찾아보는<br/>       
+                    대선후보 매칭 테스트
                 </div>
             </div>
+            <img className="intro_bg_box" src={intro_bg}>
+            </img>
+            <UnderText>전국구 스타트업 매체 SBIZ-NEWS</UnderText>
             <button onClick={nextQuestion}>시작하기</button>
         </StartPage>
     );
